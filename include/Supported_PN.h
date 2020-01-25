454d541c606c83e426e026ed6984a2aaceecce94
David Gitz <davidgitz@gmail.com>
2020-01-25 13:59:26 -0600

    Work Complete. Closed.

---
+++ include/Supported_PN.h
@@ -0,0 +1,18 @@
+// A list of all supported Part Numbers
+#define PN_100003 "100003" // Raspberry Pi 2 Model B
+#define PN_100005 "100005" // Arduino Mega 2560
+#define PN_110001 "110001" // Parallax Ping Sonar
+#define PN_110003 "110003" // AndyMark Quadrature Encoder
+#define PN_100007 "100007" // A-Star 32U4 Robot Controller SV GPIO Hat
+#define PN_110012 "110012" // Sparkfun Razor IMU
+#define PN_110013 "110013" // Adafruit LSM9DS0 IMU
+#define PN_110015 "110015" // NAVX-MXP IMU6
+#define PN_361006 "361006" // AndyMark Redline 775 Motor
+#define PN_361008 "361008" // Actuonix P16 Linear Actuator
+#define PN_362009 "362009" // Victor SPX Motor Controller
+#define PN_542026 "542026" // AndyMark 57 Sport 20:1 Gearbox
+#define PN_555005 "555005" // HobbyKing 11.1V 3S LiPo 4000mAh Battery
+#define PN_617003 "617003" // Serial 20x4 Black on RGB LCD
+#define PN_625004 "625004" // 16 Channel PWM/Servo Hat
+#define PN_625005 "625005" // Pi-EzConnect Terminal Block Breakout Terminal Hat
+#define PN_810090 "810090" // FAST Robotics Pose Cart Truth Board
