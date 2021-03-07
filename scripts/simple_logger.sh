1cc8fa5e3e3acc9ef00f44b3fe722af1e951e86d
David Gitz <davidgitz@gmail.com>
2021-03-07 15:42:45 -0600

    Work Complete. Closed.

---
+++ scripts/simple_logger.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Usage: log_message "Some Text"
+#Usage: echo "a command" | log_message
+boot_file="/home/robot/var/log/output/"$(hostname)"_log.txt"
+mkdir -p /home/robot/var/log/output/
+touch $boot_file
+sudo chown robot:robot $boot_file
+rm $boot_file
+touch $boot_file
+get_TimeStamp () { Time="["$(date +%d-%b-%Y)" "$(date +%H:%M:%S)"/"`awk '{print $1}' /proc/uptime`"]" ; }
+function log_message {
+  get_TimeStamp
+  if [ -n "$1" ]; then
+      MESSAGE="$1"
+      echo -e "${Time} $MESSAGE" | tee -a $boot_file
+  else
+      MESSAGE=$(tee)
+      echo -e "${Time} exec:$MESSAGE" >> $boot_file
+      eval $MESSAGE >> $boot_file 2>&1 &
+  fi
+}
+
+
