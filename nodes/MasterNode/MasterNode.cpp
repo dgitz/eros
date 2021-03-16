24ecbf5fe07d51ba1456f77bcb9e45cedf4ca7b8
David Gitz <davidgitz@gmail.com>
2021-03-16 08:39:44 -0500

    Work Complete, Closed.

--- nodes/MasterNode/MasterNode.cpp
+++ nodes/MasterNode/MasterNode.cpp
@@ -180,6 +180,7 @@ bool MasterNode::run_1hz() {
     }
     Diagnostic::DiagnosticDefinition diag = process->get_root_diagnostic();
     if (process->get_nodestate() == Node::State::RESET) {
+        base_reset();
         process->reset();
         logger->log_notice("Node has Reset");
         if (process->request_statechange(Node::State::RUNNING) == false) {
