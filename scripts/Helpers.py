b532a9c63f16a3b787aeb19f9776d0f0851793d1
David Gitz <davidgitz@gmail.com>
2021-06-03 04:45:06 -0500

    Updates.

---
+++ scripts/Helpers.py
@@ -0,0 +1,81 @@
+import xml.etree.ElementTree as ET
+import pdb
+import glob,os,shutil
+import json
+# Defines
+CRED = '\33[31m'
+CYELLOW = '\33[33m'
+CGREEN = '\33[32m'
+CBLUE = '\33[34m'
+CEND = '\033[0m'
+
+class Device():
+    def __init__(self,Name='',Parent='',ID=0,PartNumber='',Capability='',CatkinWS = '',Architecture='',Jobs=0):
+        self.Name = Name
+        self.Parent = Parent
+        self.ID = ID
+        self.PartNumber = PartNumber
+        self.Capability = Capability
+        self.CatkinWS = CatkinWS
+        self.Architecture = Architecture
+        self.Jobs = Jobs
+class Folder(object):
+    def __init__(self,Name='',Type='',Path='',Architectures=[]):
+        self.Name = Name
+        self.Type = Type
+        self.Directory = Path
+        self.Architectures = Architectures
+def checkDeviceFileFormat():
+    try:
+        tree = ET.parse(devicefile)
+        return True
+    except ET.ParseError as err:
+        print("XML " + devicefile + " parsing error: " + format(err))
+        return False
+def ReadSyncConfig(file_path):
+    #global devicefile
+    FolderList = []
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+    for List in root:
+        for folder in List:
+            newFolder = Folder()
+            Architectures = []
+            for entry in folder:
+                if (entry.tag == 'Name'):
+                    newFolder.Name = entry.text
+                elif (entry.tag == 'Type'):
+                    newFolder.Type = entry.text
+                elif (entry.tag == 'Directory'):
+                    newFolder.Directory = entry.text
+                elif (entry.tag == 'Architecture'):
+                    Architectures.append(entry.text)
+            newFolder.Architectures=Architectures
+            FolderList.append(newFolder)
+    return FolderList
+def ReadDeviceList(file_path,Capability):
+    #global devicefile
+    DeviceList = []
+    with open(file_path) as f:
+         data = json.load(f)
+         data = data['DeviceList']
+         for device_name in data:
+            obj = data[device_name]
+            try:
+                if(obj['Capability'] == 'ROS'):
+                    try:
+                        newDevice = Device()
+                        newDevice.Name = device_name
+                        newDevice.Capability = 'ROS'
+                        newDevice.CatkinWS = obj['CatkinWS']
+                        newDevice.Architecture = obj['Architecture']
+                        newDevice.Jobs = int(obj['Jobs'])
+                        newDevice.DeviceType = obj['Type']
+                        DeviceList.append(newDevice)
+                    except KeyError as err:
+                        print("Device: " + device_name + " Missing Key! with error: ",err)
+            except KeyError:
+                 a = 1 # Do Nothing
+    return DeviceList
+
+    
---
+++ scripts/create_from_template.py
@@ -0,0 +1,34 @@
+# Headers
+import sys,getopt,os
+from optparse import OptionParser
+import subprocess
+import pdb
+import shutil
+from cookiecutter.main import cookiecutter
+from Helpers import *
+
+
+def generate(output_folder,template_path):
+    print(CGREEN + "Generating From Template:"  + template_path  + " and storing at: " + output_folder + CEND)
+    if(os.path.isdir(output_folder) == False):
+        os.mkdir(output_folder)
+    else:
+        shutil.rmtree(output_folder)
+        os.mkdir(output_folder)
+    #pdb.set_trace()
+    os.system("cookiecutter " + template_path + " -o " + output_folder)
+    print("----------\n")
+    print(CGREEN + "Generating Content is Now Complete." + CEND)
+    os.system("cat " + template_path + "/ExtraInstructions.md")
+
+# Main
+def main():
+    parser = OptionParser("create_from_template.py [options]")
+    parser.add_option("-o","--output",dest="output_folder",default="~/tmp/",help="Output Directory,[default: %default]")
+    parser.add_option("-t","--tempate_path",dest="template_path",help="Path to Template")
+    (opts,args) = parser.parse_args()
+    generate(opts.output_folder,opts.template_path)
+    
+
+if __name__ == "__main__":
+    main()
