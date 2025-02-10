c71db4e2495856509f29791597d26c8d28640f61
David Gitz <davidgitz@gmail.com>
2025-02-10 05:48:55 -0600

    Updates

--- nodes/MasterNode/include/MasterNode.h
+++ nodes/MasterNode/include/MasterNode.h
@@ -24,29 +24,9 @@ namespace eros_nodes {
 class MasterNode : public eros::BaseNode
 {
    public:
+    MasterNode();
+    ~MasterNode();
     // Constants
-
-    // Enums
-
-    // Structs
-
-    // Initialization Functions
-
-    // Update Functions
-
-    // Attribute Functions
-
-    // Utility Functions
-
-    // Support Functions
-
-    // Message Functions
-
-    // Destructors
-
-    // Printing Functions
-
-    // TODO
     /*! \brief The base name of the Node.*/
     const std::string BASE_NODE_NAME = "master_node";
 
@@ -57,10 +37,10 @@ class MasterNode : public eros::BaseNode
     const uint16_t MINOR_RELEASE_VERSION = 2;
 
     /*! \brief The Build Number of the Node.*/
-    const uint16_t BUILD_NUMBER = 0;
+    const uint16_t BUILD_NUMBER = 1;
 
     /*! \brief A Description of the Firmware.*/
-    const std::string FIRMWARE_DESCRIPTION = "Latest Rev: 8-Feb-2025";
+    const std::string FIRMWARE_DESCRIPTION = "Latest Rev: 10-Feb-2025";
 
     /*! \brief What System this Node falls under.*/
     const eros::System::MainSystem DIAGNOSTIC_SYSTEM = eros::System::MainSystem::ROVER;
@@ -70,13 +50,16 @@ class MasterNode : public eros::BaseNode
 
     /*! \brief What Component this Node falls under.*/
     const eros::System::Component DIAGNOSTIC_COMPONENT = eros::System::Component::CONTROLLER;
-    MasterNode();
-    ~MasterNode();
-    MasterNodeProcess* get_process() {
-        return process;
-    }
+
+    // Enums
+
+    // Structs
+
+    // Initialization Functions
     bool start();
     eros::eros_diagnostic::Diagnostic finish_initialization();
+
+    // Update Functions
     bool run_loop1();
     bool run_loop2();
     bool run_loop3();
@@ -86,13 +69,27 @@ class MasterNode : public eros::BaseNode
     bool run_1hz();
     bool run_10hz();
     void thread_loop();
-    void cleanup();
 
+    // Attribute Functions
+    MasterNodeProcess* get_process() {
+        return process;
+    }
+
+    // Utility Functions
+
+    // Support Functions
+
+    // Message Functions
     bool changenodestate_service(eros::srv_change_nodestate::Request& req,
                                  eros::srv_change_nodestate::Response& res);
     bool device_service(eros::srv_device::Request& req, eros::srv_device::Response& res);
     void system_commandAction_Callback(const eros::system_commandGoalConstPtr& goal);
     void command_Callback(const eros::command::ConstPtr& t_msg);
+
+    // Destructors
+    void cleanup();
+
+    // Printing Functions
 
    private:
     eros::eros_diagnostic::Diagnostic read_launchparameters();
--- nodes/MasterNode/include/MasterNodeProcess.h
+++ nodes/MasterNode/include/MasterNodeProcess.h
@@ -9,6 +9,8 @@ namespace eros_nodes {
 class MasterNodeProcess : public eros::BaseNodeProcess
 {
    public:
+    MasterNodeProcess();
+    ~MasterNodeProcess();
     // Constants
 
     // Enums
@@ -16,33 +18,29 @@ class MasterNodeProcess : public eros::BaseNodeProcess
     // Structs
 
     // Initialization Functions
+    eros::eros_diagnostic::Diagnostic finish_initialization();
+    void reset();
 
     // Update Functions
+    eros::eros_diagnostic::Diagnostic update(double t_dt, double t_ros_time);
 
     // Attribute Functions
 
     // Utility Functions
 
     // Support Functions
+    std::vector<eros::eros_diagnostic::Diagnostic> check_programvariables();
 
     // Message Functions
+    std::vector<eros::eros_diagnostic::Diagnostic> new_commandmsg(eros::command msg);
 
     // Destructors
-
-    // Printing Functions
-
-    // TODO
-    MasterNodeProcess();
-    ~MasterNodeProcess();
-    eros::eros_diagnostic::Diagnostic finish_initialization();
-    void reset();
-    eros::eros_diagnostic::Diagnostic update(double t_dt, double t_ros_time);
-    std::vector<eros::eros_diagnostic::Diagnostic> new_commandmsg(eros::command msg);
-    std::vector<eros::eros_diagnostic::Diagnostic> check_programvariables();
     void cleanup() {
         base_cleanup();
         return;
     }
+
+    // Printing Functions
 
    private:
 };
